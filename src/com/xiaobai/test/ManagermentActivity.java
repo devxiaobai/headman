2ad4ba8146cf635834840e76c8338aa97c58e3c3
baiyingjun <yingjun.bai@renren-inc.com>
2012-09-11 19:03:01 +0800

    add src

---
+++ src/com/xiaobai/test/ManagermentActivity.java
@@ -0,0 +1,13 @@
+package com.xiaobai.test;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ManagermentActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(R.layout.management);
+	}
+}
---
+++ src/com/xiaobai/test/MyGridView.java
@@ -0,0 +1,40 @@
+package com.xiaobai.test;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.GridView;
+
+/**
+ * This class is inherit from GridView, overriding onMeasure method to 
+ * remove scroll function in this gridview , and to avoid only one row 
+ * displayed when embedded in a ScrollView control.
+ * 
+* @author yingjun.bai@renren-inc.com
+* @date 2012-8-27 上午11:03:26
+ */
+public class MyGridView extends GridView {
+	public MyGridView(Context context) {
+		super(context);
+	}
+
+	public MyGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	@Override  
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {  
+	    int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,  
+	            MeasureSpec.AT_MOST);  
+	    super.onMeasure(widthMeasureSpec, expandSpec);  
+	  
+	}  
+	
+	// 禁止Grid滑动
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		if(ev.getAction() == MotionEvent.ACTION_MOVE)
+			return true;
+		return super.dispatchTouchEvent(ev);
+	}
+}
